00dca50c6104d37e0b305be1fa9e61346b031c04
Three97 <jason.fedler@gmail.com>
2021-09-08 18:38:49 -0400

    adds shared array for minimizing duplicate dependencies;

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -16,7 +16,8 @@ const devConfig = {
       name: 'container',
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-      }
+      },
+      shared: [ 'react', 'react-dom' ]
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
