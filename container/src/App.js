4507c0ef9a1a9f65f82e86e1467d9a05229f7d99
Three97 <jason.fedler@gmail.com>
2021-10-05 19:40:30 -0400

    changes the production prefix for the js-to-css results

--- container/src/App.js
+++ container/src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { BrowserRouter  } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'con'
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
