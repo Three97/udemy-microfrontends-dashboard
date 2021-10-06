3f9fbb87677e932f6a1ef98b42001819591b7fd4
Three97 <jason.fedler@gmail.com>
2021-10-05 20:48:09 -0400

    adds two-way history sync between container and marketing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(
-    <App />,
+    <App history={history} />,
     el
   );
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  };
 };
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.getElementById('_marketing-dev-root');
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
