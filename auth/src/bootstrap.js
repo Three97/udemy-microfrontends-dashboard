8acff3b00cfcc3151b8274a4c9591b8dec6cba9d
Three97 <jason.fedler@gmail.com>
2021-10-10 10:09:18 -0400

    corrects bug related to history and auth app (and apply to marketing app)

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [ initialPath ]
+  });
   if (onNavigate) {
     history.listen(onNavigate);
   }
@@ -17,6 +19,7 @@ const mount = (el, { onNavigate, defaultHistory }) => {
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
+      
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
